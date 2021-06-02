fc4146885014215b87f76ffd830ca6bff1b8d59d
Soumay kanti Das <anikds2859@outlook.com>
2021-06-02 13:57:40 +0600

    Add project files.

---
+++ Foreach_Example/Program.cs
@@ -0,0 +1,17 @@
+﻿// cs_foreach.cs
+using System;
+
+namespace Foreach_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Fibarrray = new int[] { 0, 1, 2, 3, 5, 8, 13 };
+            foreach (int i in Fibarrray)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
